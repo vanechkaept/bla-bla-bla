014855d8d8418481737545d8ac14fa16e9a671c7
ivan.kolmak@stoloto.ru <ivan.kolmak@stoloto.ru>
2020-02-03 17:52:12 +0300

    List with divider

--- anuglar-material/src/app/material/material.module.ts
+++ anuglar-material/src/app/material/material.module.ts
@@ -9,6 +9,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 
@@ -23,6 +25,8 @@ const MaterialCoponents = [
   MatToolbarModule,
   MatSidenavModule,
   MatMenuModule,
+  MatListModule,
+  MatDividerModule,
 
 
 ];
