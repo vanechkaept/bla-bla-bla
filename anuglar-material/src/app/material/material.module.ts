436846be6d5adfed5021ff06a935ed2c3ebcff79
ivan.kolmak@stoloto.ru <ivan.kolmak@stoloto.ru>
2020-02-04 11:17:19 +0300

    Panel according/expansion

--- anuglar-material/src/app/material/material.module.ts
+++ anuglar-material/src/app/material/material.module.ts
@@ -12,6 +12,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatListModule} from '@angular/material/list';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 
 
@@ -29,6 +30,7 @@ const MaterialCoponents = [
   MatListModule,
   MatDividerModule,
   MatGridListModule,
+  MatExpansionModule
 
 ];
 
